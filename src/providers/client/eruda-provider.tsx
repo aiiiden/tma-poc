f51eb16f211492def470247e93e84d82d10df9b5
aiiiden <hjjam100@gmail.com>
2024-10-14 18:36:30 +0900

    fix: build error

--- src/providers/client/eruda-provider.tsx
+++ src/providers/client/eruda-provider.tsx
@@ -1,11 +1,10 @@
 'use client';
 
-import eruda from 'eruda';
 import { useEffect } from 'react';
 
 export const ErudaProvider = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
-    eruda.init();
+    import('eruda').then((lib) => lib.default.init());
   }, []);
 
   return children;
