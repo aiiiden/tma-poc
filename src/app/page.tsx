d7b9d5728553da3a1ac69fb0df91c70a13ec3764
aiiiden <hjjam100@gmail.com>
2024-10-15 14:44:13 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,26 +12,28 @@ export default function Home() {
     <div>
       <header className="space-y-1 py-6 px-2 text-center">
         <h1 className="text-2xl font-bold">TMA POC</h1>
-        {tg.launchParams?.initDataRaw && (
-          <Link
-            target="_blank"
-            className={buttonVariants({
-              variant: 'outline',
-              size: 'sm',
-              className: 'text-xs',
-            })}
-            href={location.href}
-          >
-            Open in browser
-          </Link>
-        )}
       </header>
-
-      <div className="p-3 border-y">
-        <pre className="text-left text-[10px] p-4 bg-gray-200 rounded-lg overflow-x-auto">
-          {JSON.stringify(tg, null, 2)}
-        </pre>
-      </div>
+      <main className="border-b">
+        <div className="p-4 border-t space-y-1">
+          <h2 className="text-lg font-medium">Location</h2>
+          <pre
+            role="button"
+            onClick={() => {
+              navigator.clipboard.writeText(location.href);
+              alert('Copied to clipboard');
+            }}
+            className="text-left text-[10px] p-4 bg-gray-200 rounded-lg overflow-x-auto"
+          >
+            {location.href}
+          </pre>
+        </div>
+        <div className="p-4 border-t space-y-1">
+          <h2 className="text-lg font-medium">useTelegram Hook</h2>
+          <pre className="text-left text-[10px] p-4 bg-gray-200 rounded-lg overflow-x-auto">
+            {JSON.stringify(tg, null, 2)}
+          </pre>
+        </div>
+      </main>
     </div>
   );
 }
