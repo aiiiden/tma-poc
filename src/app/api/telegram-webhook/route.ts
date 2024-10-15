f14e6698fe29fadb1d502507add09869bf0330a6
aiiiden <hjjam100@gmail.com>
2024-10-15 16:41:51 +0900

    chore: add retrier

--- src/app/api/telegram-webhook/route.ts
+++ src/app/api/telegram-webhook/route.ts
@@ -1,3 +1,4 @@
+import { autoRetry } from '@grammyjs/auto-retry';
 import { Bot, InputMediaBuilder, webhookCallback } from 'grammy';
 
 export const dynamic = 'force-dynamic';
@@ -7,6 +8,8 @@ export const fetchCache = 'force-no-store';
 const TELEGRAM_BOT_TOKEN = '7580765678:AAEArN0V0IOJlTXXcMLXp5mADY-KO_2DJF4';
 
 const bot = new Bot(TELEGRAM_BOT_TOKEN);
+
+bot.api.config.use(autoRetry());
 
 const photo = InputMediaBuilder.photo(
   'https://i.ibb.co/bJmr26G/2024-09-16-21-55-29.jpg',
