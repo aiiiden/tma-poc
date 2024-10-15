eec77241bb2aeafb08eae823e3f13960e24d2191
aiiiden <hjjam100@gmail.com>
2024-10-15 16:27:58 +0900

    chore: test grammy with webhook

---
+++ src/app/api/telegram-webhook/route.ts
@@ -0,0 +1,22 @@
+import { Bot, InputMediaBuilder, webhookCallback } from 'grammy';
+
+export const dynamic = 'force-dynamic';
+
+export const fetchCache = 'force-no-store';
+
+const TELEGRAM_BOT_TOKEN = '7580765678:AAEArN0V0IOJlTXXcMLXp5mADY-KO_2DJF4';
+
+const bot = new Bot(TELEGRAM_BOT_TOKEN);
+
+const photo = InputMediaBuilder.photo(
+  'https://i.ibb.co/bJmr26G/2024-09-16-21-55-29.jpg',
+  {
+    caption: 'This is bommy, sent from a bot! *START PLAY*',
+  },
+);
+
+bot.on('message:text', async (ctx) => {
+  await ctx.replyWithMediaGroup([photo]);
+});
+
+export const POST = webhookCallback(bot, 'std/http');
